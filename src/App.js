2407ed688f69ce3c2f502d8253f9a36b4252a326
Ismail Zouari <ismail.zr2002@gmail.com>
2023-03-25 12:27:39 +0100

    state-check

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person: {
+      fullName: "Ismail Zouari",
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula neque ac ante ultrices, vel volutpat eros faucibus. Nam viverra massa vel magna vehicula, sed auctor sapien rhoncus. Suspendisse mollis, turpis in bibendum vestibulum, mauris dolor tempor erat, a posuere nulla odio et sem.",
+      imgSrc:"Profil.png",
+      profession: "Web Developer"
+    },
+    shows: true,
+    intervalId: null,
+    mountedAt: new Date()
+  }
+
+  componentDidMount() {
+    const intervalId = setInterval(() => {
+      this.setState({ mountedAt: new Date() })
+    }, 1000)
+    this.setState({ intervalId })
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId)
+  }
+
+  toggleShows = () => {
+    this.setState({ shows: !this.state.shows })
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.toggleShows}>Toggle Profile</button>
+        {this.state.shows &&
+          <div>
+            <img src={this.state.person.imgSrc} alt={this.state.person.fullName} />
+            <h1>{this.state.person.fullName}</h1>
+            <p>{this.state.person.bio}</p>
+            <p>{this.state.person.profession}</p>
+          </div>
+        }
+        <p>Component mounted at: {this.state.mountedAt.toLocaleString()}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
